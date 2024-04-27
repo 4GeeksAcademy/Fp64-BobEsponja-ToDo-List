edf0eab567111f168d625255630623bae20b82b2
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-04-27 16:28:50 +0200

    Funcionalidad componentes check

---
+++ bobesponja-todo-list/src/components/Categorias/Categorias.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Categorias({textoCategoria, setCategorias}) {
+ 
+const setClass = () =>{
+
+    return textoCategoria === 'My Day' ? 'mydaycateg' :
+       textoCategoria === 'Personal' ? 'personalcateg' :
+       textoCategoria === 'All' ? 'allcateg' :
+       textoCategoria === 'Important' ? 'importantcateg' :
+       undefined; 
+
+}
+const setClassIcono =()=>{
+    return textoCategoria === 'My Day' ? 'bolt' :
+    textoCategoria === 'Personal' ? 'person' :
+    textoCategoria === 'All' ? 'dashboard' :
+    textoCategoria === 'Important' ? 'stars' :
+    undefined; 
+
+}
+
+const setDiv =()=>{
+    return textoCategoria === 'My Day' ? 'mydaydiv' :
+       textoCategoria === 'Personal' ? 'personaldiv' :
+       textoCategoria === 'All' ? 'alldiv' :
+       textoCategoria === 'Important' ? 'importantdiv' :
+       undefined; 
+
+}
+
+const setNewCategoryState =()=>{
+    setCategorias(textoCategoria)
+
+}
+  return (
+   
+      <div className={setDiv()}> 
+        <span className="material-symbols-outlined">{setClassIcono()}</span>
+        <a href="#" className={setClass()} onClick={setNewCategoryState}>{textoCategoria}</a>
+        </div>
+    
+  )
+}
--- bobesponja-todo-list/src/components/Sidebar/FirstTop.jsx
+++ bobesponja-todo-list/src/components/Sidebar/FirstTop.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './FirstTop.css'
 import { useState } from 'react'
 import Logo from '../../assets/Logo.svg'
 
 
-export default function FirstTop() {
+export default function FirstTop({ categorias }) {
+  const [inputState, setInputState] = useState('')
+  const [taskListState, setTaskListState] = useState([]);
+  const [filteredTaskListState, setFilteredTaskListState] = useState([]);
+  const [filteredTaskCategoryState, setFilteredTaskCategoryState] = useState([]);
 
-    // const [tasks, setTasks] = useState([]);
-    // const [search, setSearch] = useState("");
-  
-    // const addTask = task => {
-    //   if (task) {
-    //     setTasks([...tasks, task]);
-    //   }
-    // };
-  
-    // const handleSearch = event => {
-    //   setSearch(event.target.value);
-    // };
-  
-    // const filteredTasks = tasks.filter(task =>
-    //   task.toLowerCase().includes(search.toLowerCase())
-    // );
-    // value={search} onChange={handleSearch}
-    
+
+  useEffect(() => {
+        setTaskListState([]);
+      },[]);
+
+  useEffect(()=>{
+    inputState === '' ? setFilteredTaskListState(taskListState) : setFilteredTaskListState(taskListState.filter((task) => { return task.nombre.toLowerCase().includes(inputState.toLowerCase())}));
+  },[taskListState, inputState ])
+
+  useEffect( () => {
+    // console.log(taskListState)
+    categorias === '' ? setFilteredTaskCategoryState(taskListState) : 
+    setFilteredTaskCategoryState(taskListState.filter((task) => { return task.categoria.toLowerCase().includes(categorias.toLowerCase())}))
+  },[taskListState, categorias ])
+
   return (
     <div>
        <div className='firstTop'>
        <img className="logo" src={Logo}></img>
          <div className='search'>
             <span className="material-symbols-outlined lupa">search</span>
-                 <input type="text" id="search"  placeholder='Search' ></input>
+                 <input type="text" id="search"  placeholder='Search' onChange={event => setInputState(event.target.value)} value={inputState} ></input>
          </div>
          <span className='categories'>Categories</span>
         </div>
